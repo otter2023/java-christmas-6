d107d6d6624c93e1f4e8fd538021ad15c9d70f13
Runea <otter2023@naver.com>
2025-12-31 00:45:14 +0900

    feat: 예외 처리

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -28,6 +28,11 @@ public class InputView {
     public Map<Menu, Integer> readMenu() {
         System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
         String menuLineUp = Console.readLine();
+
+        if (!menuLineUp.matches("^0-9[ㄱ-ㅎ|가-힣]*$")) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+
         return Parser.splitMenu(menuLineUp);
     }
 
